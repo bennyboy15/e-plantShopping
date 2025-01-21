4f706e9d3719fda3da9f0624078d73e66094fed6
BenJHarves <73825010+bennyboy15@users.noreply.github.com>
2025-01-21 15:00:21 +1030

    Task 5 - Part 2

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -2,6 +2,7 @@ import React, { useState,useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
 function ProductList() {
+
     const [showCart, setShowCart] = useState(false); 
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
